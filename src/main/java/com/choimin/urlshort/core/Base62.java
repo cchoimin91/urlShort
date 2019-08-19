4cb7378c79e5f3e814cb81ba6aa5ec463bd6aa50
choimin <cchoimin1991@gmail.com>
2019-08-19 20:33:29 +0900

    디코딩

--- src/main/java/com/choimin/urlshort/core/Base62.java
+++ src/main/java/com/choimin/urlshort/core/Base62.java
@@ -6,23 +6,50 @@ public class Base62 {
 
 
     /**
-     *
-     * @param dbKey DB키값을 입력합니다.
-     * @return 62진수로 인코딩된 문자열을 반환합니다.
+     * DB키값을 62진수로 인코딩 합니다.
+     * @param dbKey DB키값
+     * @return 62진수로 인코딩된 문자열
      */
     public static String encode(long dbKey) {
         if (dbKey < 0) {
             throw new IllegalArgumentException("dbkey는 0보다 커야 합니다.");
         }
 
-        StringBuffer sb = new StringBuffer();
+        String encodeValue = "";
 
         while (dbKey > 0) {
-            sb.append(BASE62_CHARACTERS.charAt((int) (dbKey % 62))).append(sb);
-            dbKey = dbKey / 62;
+            encodeValue = BASE62_CHARACTERS.charAt((int) (dbKey % 62)) + encodeValue;
+            dbKey /= 62;
         }
 
-        return sb.toString();
+        return encodeValue;
+    }
+
+
+    /**
+     * 인코딩된 문자열을 디코딩 합니다
+     * @param base62Encode base62로 인코딩된 문자열
+     * @return DBKey값
+     */
+    public static long decode(String base62Encode) {
+        for (char character : base62Encode.toCharArray()) {
+            if (!BASE62_CHARACTERS.contains(String.valueOf(character))) {
+                throw new IllegalArgumentException("BASE62_CHARACTERS가 아닙니다 input: " + character);
+            }
+        }
+
+        long dbKey = 0;
+
+        base62Encode = new StringBuffer(base62Encode).reverse().toString();
+
+        long count = 1;
+
+        for (char character : base62Encode.toCharArray()) {
+            dbKey += BASE62_CHARACTERS.indexOf(character) * count;
+            count *= 62;
+        }
+
+        return dbKey;
     }
 
 }
